8f919be8443e8e944dc0b005c544663104ec53eb
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-05-29 13:30:35 +0300

    CREATE BACKEND env for Cypress tests

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:5173'
+    baseUrl: 'http://localhost:5173',
   },
   env: {
-    BACKEND: 'http://localhost:3003/api'
-  }
+    BACKEND: process.env.BACKEND,
+  },
 })
